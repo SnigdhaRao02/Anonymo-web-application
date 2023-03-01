cdceff3eebca05eee0277a7eff5bf5fb742190a1
SnigdhaRao02 <kotharusnigdha2002@gmail.com>
2023-03-01 19:52:18 +0530

    #

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/anonymo"
+    callbackURL: "http://anonymo.cyclic.app/auth/google/anonymo"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
